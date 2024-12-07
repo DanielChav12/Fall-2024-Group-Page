bdb6394624013f772311dee69ce360294e46bbd1
DanielChav12 <Yellowjacketblue12@gmail.com>
2024-12-07 11:48:26 -0600

    added JS

---
+++ js/gallery.js
@@ -0,0 +1,30 @@
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    // console.log( "ready!" );
+ 
+ 
+    $("#galleryArtofLife").nanogallery2({
+ 
+     thumbnailHeight: "auto",
+     thumbnailWidth:  "450",
+     thumbnailBorderVertical: "0",
+     thumbnailBorderHorizontal: "0",
+     thumnailLabel: {
+         position: "overImageOnBottom",
+         display: false
+     },
+     thumbnailHoverEffect2: "imageScaleIn80|imageSepiaOff|LabelAppear75",
+     thumbnailGutterWidth: 20,
+     thumbnailGutterHeight: 20,
+     thumbnailAlignment: "center",
+     thumbnailOpenImage: true
+ 
+ 
+   });
+ 
+ 
+ 
+ 
+ });
+ 
